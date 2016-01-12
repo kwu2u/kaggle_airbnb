e172cbc177d8b1ea855ed1c684fe9e246fe29a94
kevinwu06 <kevinxwu@gmail.com>
2016-01-12 13:51:01 -0500

    started data exploration notebook
    also implemented ndcg5 in CV

--- script/random_search.py
+++ script/random_search.py
@@ -5,7 +5,9 @@ Created on Mon Dec 28 15:26:12 2015
 @author: kwu
 """
 
+import numpy as np
 import xgboost as xgb
+'''
 from sklearn.grid_search import RandomizedSearchCV 
 
 # Grid search hyperparameter tuning
@@ -38,12 +40,45 @@ print rand.best_score_
 print rand.best_params_
 
 # 0.608678338354
-
 '''
-param = {'eta': 0.15, 'max_depth': 6,'subsample': 0.5, 'colsample_bytree': 0.5, 
-         'objective': 'multi:softprob', 'num_class': 12}
-num_round = 45
+# run preprocessing in kwu2u_airbnb.py   
+
+xgb_params = {'eta': 0.05, 
+              'max_depth': 6,
+              'subsample': 0.7, 
+              'colsample_bytree': 0.7, 
+              'objective': 'multi:softprob', 
+              'num_class': 12,
+              'eval_metric':'ndcg',
+              'seed':1234}
+              
+def customized_eval(preds, dtrain):
+    labels = dtrain.get_label()
+    top = []
+    for i in range(preds.shape[0]):
+        top.append(np.argsort(preds[i])[::-1][:5])
+    mat = np.reshape(np.repeat(labels, 
+                               np.shape(top)[1]) == np.array(top).ravel(),
+                               np.array(top).shape
+                               ).astype(int)
+    score = np.mean(np.sum(mat/np.log2(np.arange(2, mat.shape[1] + 2)),axis = 1))
+    return 'ndcg5', score
+
 label2num = {label: i for i, label in enumerate(sorted(set(y)))}
 dtrain = xgb.DMatrix(X, label=[label2num[label] for label in y])
-xgb.cv(param, dtrain, num_round, nfold=3, metrics={'ndcg'}, seed = 0)
+cv = xgb.cv(xgb_params, dtrain, num_boost_round=200, nfold=3, seed=0, 
+             feval = customized_eval, maximize = True, show_progress = True)
+
+'''
+cv-test-ndcg5:0.832291666667
+xgb_params = {'eta': 0.05, 
+              'max_depth': 6,
+              'subsample': 0.7, 
+              'colsample_bytree': 0.7, 
+              'objective': 'multi:softprob', 
+              'num_class': 12,
+              'eval_metric':'ndcg',
+              'seed':1234}
+num_boost_round=200
+nfold=3
 '''
